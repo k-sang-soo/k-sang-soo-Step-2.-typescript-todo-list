e59e7bd31e3fc7603c33bdb0e74cb7f055401980
k-sang-soo <wsckm1@google.com>
2022-10-04 21:45:37 +0900

    react-hook-form 심화과정

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
-import { useForm } from "react-hook-form";
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
 
 interface IFormData {
-  Email: string;
-  FirstName: string;
-  LastName: string;
-  Username: string;
-  Password: string;
-  PasswordConfirm: string;
+    Email: string;
+    FirstName: string;
+    LastName: string;
+    Username: string;
+    Password: string;
+    PasswordConfirm: string;
+    extraError?: string;
 }
 
 // function ToDoList() {
@@ -45,62 +46,82 @@ interface IFormData {
 // }
 
 function ToDoList() {
-  const {
-    register,
-    watch,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<IFormData>();
+    const {
+        register,
+        watch,
+        handleSubmit,
+        formState: { errors },
+        setError,
+    } = useForm<IFormData>({
+        defaultValues: {
+            Email: '@naver.com',
+        },
+    });
 
-  const onValid = (data: any) => {
-    console.log("handleSubmit", data);
-  };
-  console.log(errors);
-  // console.log(watch()); form에 입력 값을 보여줌
-  return (
-    <div>
-      <form onSubmit={handleSubmit(onValid)}>
-        <input
-          {...register("Email", {
-            required: "필수 입력 값",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
-              message: "네이버 메일만 허용 됩니다.",
-            },
-          })}
-          placeholder="Email"
-        />
-        <span>{errors?.Email?.message}</span>
-        <input
-          {...register("FirstName", { required: "필수 입력 값" })}
-          placeholder="First Name"
-        />
-        <input
-          {...register("LastName", { required: "필수 입력 값" })}
-          placeholder="Last Name"
-        />
-        <input
-          {...register("Username", { required: "필수 입력 값" })}
-          placeholder="Username"
-        />
-        <input
-          {...register("Password", {
-            required: "필수 입력 값",
-            minLength: { value: 5, message: "패스워드 길이가 짧습니다." },
-          })}
-          placeholder="Password"
-        />
-        <input
-          {...register("PasswordConfirm", {
-            required: "필수 입력 값",
-            minLength: { value: 5, message: "패스워드 길이가 짧습니다." },
-          })}
-          placeholder="PasswordConfirm"
-        />
-        <button>Add</button>
-      </form>
-    </div>
-  );
+    const onValid = (data: IFormData) => {
+        console.log(setError);
+
+        if (data.Password !== data.PasswordConfirm) {
+            //shouldFocus 오류 발생 시 해당 위치로 이동
+            setError('PasswordConfirm', { message: '패스워드가 일치하지 않습니다.' }, { shouldFocus: true });
+        }
+
+        // if(state === '404') {
+        //   setError('extraError', { message: '서버 상태가 원할하지 않습니다.' });
+        // }
+    };
+    console.log(errors);
+    // console.log(watch()); form에 입력 값을 보여줌
+    return (
+        <div>
+            <form onSubmit={handleSubmit(onValid)} style={{ display: 'flex', flexDirection: 'column' }}>
+                <input
+                    {...register('Email', {
+                        required: '필수 입력 값',
+                        pattern: {
+                            value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+                            message: '네이버 메일만 허용 됩니다.',
+                        },
+                    })}
+                    placeholder="Email"
+                />
+                <span>{errors?.Email?.message}</span>
+                <input
+                    {...register('FirstName', {
+                        required: '필수 입력 값',
+                        validate: {
+                            noNico: (value) => (value.includes('nico') ? 'nico 금지된 단어 입니다.' : true),
+                            noNick: (value) => (value.includes('nick') ? 'nick 금지된 단어 입니다.' : true),
+                        },
+                    })}
+                    placeholder="First Name"
+                />
+                <span>{errors?.FirstName?.message}</span>
+                <input {...register('LastName', { required: '필수 입력 값' })} placeholder="Last Name" />
+                <span>{errors?.LastName?.message}</span>
+                <input {...register('Username', { required: '필수 입력 값' })} placeholder="Username" />
+                <span>{errors?.Username?.message}</span>
+                <input
+                    {...register('Password', {
+                        required: '필수 입력 값',
+                        minLength: { value: 5, message: '패스워드 길이가 짧습니다.' },
+                    })}
+                    placeholder="Password"
+                />
+                <span>{errors?.Password?.message}</span>
+                <input
+                    {...register('PasswordConfirm', {
+                        required: '필수 입력 값',
+                        minLength: { value: 5, message: '패스워드 길이가 짧습니다.' },
+                    })}
+                    placeholder="PasswordConfirm"
+                />
+                <span>{errors?.PasswordConfirm?.message}</span>
+                <button>Add</button>
+                <span>{errors?.extraError?.message}</span>
+            </form>
+        </div>
+    );
 }
 
 export default ToDoList;
