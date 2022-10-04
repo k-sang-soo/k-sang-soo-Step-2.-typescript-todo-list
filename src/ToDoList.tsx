6f26ff0290c9278a43cc1677d982736e8bc9e66d
k-sang-soo <67569730+k-sang-soo@users.noreply.github.com>
2022-10-04 17:01:17 +0900

    feat : react-hook-form 연습

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,6 +1,15 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
+interface IFormData {
+  Email: string;
+  FirstName: string;
+  LastName: string;
+  Username: string;
+  Password: string;
+  PasswordConfirm: string;
+}
+
 // function ToDoList() {
 //   const [toDo, setTodo] = useState("");
 //   const [toDoError, setToDoError] = useState("");
@@ -36,17 +45,58 @@ import { useForm } from "react-hook-form";
 // }
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch()); // form에 입력 값을 보여줌
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IFormData>();
+
+  const onValid = (data: any) => {
+    console.log("handleSubmit", data);
+  };
+  console.log(errors);
+  // console.log(watch()); form에 입력 값을 보여줌
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
-        <input {...register("First Name")} placeholder="First Name" />
-        <input {...register("Last Name")} placeholder="Last Name" />
-        <input {...register("Username")} placeholder="Username" />
-        <input {...register("Password")} placeholder="Password" />
-        <input {...register("PasswordConfirm")} placeholder="PasswordConfirm" />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input
+          {...register("Email", {
+            required: "필수 입력 값",
+            pattern: {
+              value: /^[A-Za-z0-9._%+-]+@naver\.com$/,
+              message: "네이버 메일만 허용 됩니다.",
+            },
+          })}
+          placeholder="Email"
+        />
+        <span>{errors?.Email?.message}</span>
+        <input
+          {...register("FirstName", { required: "필수 입력 값" })}
+          placeholder="First Name"
+        />
+        <input
+          {...register("LastName", { required: "필수 입력 값" })}
+          placeholder="Last Name"
+        />
+        <input
+          {...register("Username", { required: "필수 입력 값" })}
+          placeholder="Username"
+        />
+        <input
+          {...register("Password", {
+            required: "필수 입력 값",
+            minLength: { value: 5, message: "패스워드 길이가 짧습니다." },
+          })}
+          placeholder="Password"
+        />
+        <input
+          {...register("PasswordConfirm", {
+            required: "필수 입력 값",
+            minLength: { value: 5, message: "패스워드 길이가 짧습니다." },
+          })}
+          placeholder="PasswordConfirm"
+        />
         <button>Add</button>
       </form>
     </div>
