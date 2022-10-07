d55032632dc962204391e4c4458e42039e9aec6c
k-sang-soo <67569730+k-sang-soo@users.noreply.github.com>
2022-10-07 17:55:10 +0900

    feat : to-do-list 삭제 버튼 제작 중...

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,32 +1,36 @@
-import { atom, selector } from 'recoil';
+import { atom, selector } from "recoil";
+
+export enum toDoStorage {
+  "TODOS_STORAGE" = "TODOS_STORAGE",
+}
 
 export enum Categories {
-    'TO_DO' = 'TO_DO',
-    'DOING' = 'DOING',
-    'DONE' = 'DONE',
+  "TO_DO" = "TO_DO",
+  "DOING" = "DOING",
+  "DONE" = "DONE",
 }
 
 export interface IToDoData {
-    id: number;
-    text: string;
-    category: Categories;
+  id: number;
+  text: string;
+  category: Categories;
 }
 
 export const categoryState = atom<Categories>({
-    key: 'category',
-    default: Categories.TO_DO,
+  key: "category",
+  default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDoData[]>({
-    key: 'toDo',
-    default: [],
+  key: "toDo",
+  default: JSON.parse(localStorage.getItem(toDoStorage.TODOS_STORAGE) as any),
 });
 
 export const toDoSelector = selector({
-    key: 'toDoSelector',
-    get: ({ get }) => {
-        const toDos = get(toDoState);
-        const category = get(categoryState);
-        return toDos.filter((toDo) => toDo.category === category);
-    },
+  key: "toDoSelector",
+  get: ({ get }) => {
+    const toDos = get(toDoState);
+    const category = get(categoryState);
+    return toDos.filter((toDo) => toDo.category === category);
+  },
 });
--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,86 +1,90 @@
-import React from 'react';
-import { Categories, IToDoData, toDoState } from '../atoms';
-import { useSetRecoilState } from 'recoil';
-import { log } from 'util';
+import React from "react";
+import { Categories, IToDoData, toDoState, toDoStorage } from "../atoms";
+import { useSetRecoilState } from "recoil";
+import { log } from "util";
 
 function ToDo({ text, category, id }: IToDoData) {
-    const setToDos = useSetRecoilState(toDoState);
-    const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        const {
-            currentTarget: { name },
-        } = event;
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
 
-        setToDos((oldToDos) => {
-            console.log('oldToDos', oldToDos);
-            return oldToDos.map((toDo) => (toDo.id === id ? { ...toDo, category: name as any } : toDo));
-        });
-    };
+    setToDos((oldToDos) => {
+      console.log("oldToDos", oldToDos);
+      return oldToDos.map((toDo) =>
+        toDo.id === id ? { ...toDo, category: name as any } : toDo
+      );
+    });
+  };
 
-    const DeleteToDo = () => {
-        setToDos((oldToDos) => {
-            return oldToDos.filter((toDo) => toDo.id !== id);
-        });
-    };
+  const DeleteToDo = () => {
+    setToDos((oldToDos) => {
+      const result = oldToDos.filter((toDo) => toDo.id !== id);
+      localStorage.setItem(toDoStorage.TODOS_STORAGE, JSON.stringify(result));
+      return result;
+    });
+  };
 
-    return (
-        <li style={{ marginTop: '8px' }}>
-            <span style={{ marginRight: '8px' }}>{text}</span>
+  return (
+    <li style={{ marginTop: "8px" }}>
+      <span style={{ marginRight: "8px" }}>{text}</span>
 
-            {category !== Categories.TO_DO && (
-                <button
-                    name={Categories.TO_DO}
-                    onClick={onClick}
-                    style={{
-                        marginRight: '8px',
-                        padding: '4px',
-                        border: '1px solid #FFF',
-                        borderRadius: '4px',
-                    }}
-                >
-                    Doing
-                </button>
-            )}
-            {category !== Categories.DOING && (
-                <button
-                    name={Categories.DOING}
-                    onClick={onClick}
-                    style={{
-                        marginRight: '8px',
-                        padding: '4px',
-                        border: '1px solid #FFF',
-                        borderRadius: '4px',
-                    }}
-                >
-                    To Do
-                </button>
-            )}
-            {category !== Categories.DONE && (
-                <button
-                    name={Categories.DONE}
-                    onClick={onClick}
-                    style={{
-                        marginRight: '8px',
-                        padding: '4px',
-                        border: '1px solid #FFF',
-                        borderRadius: '4px',
-                    }}
-                >
-                    Done
-                </button>
-            )}
-            <button
-                onClick={DeleteToDo}
-                style={{
-                    marginRight: '8px',
-                    padding: '4px',
-                    border: '1px solid #FFF',
-                    borderRadius: '4px',
-                }}
-            >
-                Delete
-            </button>
-        </li>
-    );
+      {category !== Categories.TO_DO && (
+        <button
+          name={Categories.TO_DO}
+          onClick={onClick}
+          style={{
+            marginRight: "8px",
+            padding: "4px",
+            border: "1px solid #FFF",
+            borderRadius: "4px",
+          }}
+        >
+          TO_DO
+        </button>
+      )}
+      {category !== Categories.DOING && (
+        <button
+          name={Categories.DOING}
+          onClick={onClick}
+          style={{
+            marginRight: "8px",
+            padding: "4px",
+            border: "1px solid #FFF",
+            borderRadius: "4px",
+          }}
+        >
+          DOING
+        </button>
+      )}
+      {category !== Categories.DONE && (
+        <button
+          name={Categories.DONE}
+          onClick={onClick}
+          style={{
+            marginRight: "8px",
+            padding: "4px",
+            border: "1px solid #FFF",
+            borderRadius: "4px",
+          }}
+        >
+          Done
+        </button>
+      )}
+      <button
+        onClick={DeleteToDo}
+        style={{
+          marginRight: "8px",
+          padding: "4px",
+          border: "1px solid #FFF",
+          borderRadius: "4px",
+        }}
+      >
+        Delete
+      </button>
+    </li>
+  );
 }
 
 // click event 에서 새 인자를 받는 다른 방법
