814bc751caa1492e95066b6f3b685b4a51b2591b
k-sang-soo <wsckm1@google.com>
2022-10-10 14:48:29 +0900

    저장하거나 삭제 될 때 로컬스토리지에 저장하는 함수를 각각에다 적용 시키지 않고 useEffect를 사용해 값이 변경되는 걸 감지하면 로컬스토리지에 저장함으로서 한 곳에서 모든 걸 처리 할 수있게 코드 최소화

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,36 +1,38 @@
-import { atom, selector } from "recoil";
+import { atom, selector } from 'recoil';
 
 export enum toDoStorage {
-  "TODOS_STORAGE" = "TODOS_STORAGE",
+    'TODOS_STORAGE' = 'TODOS_STORAGE',
 }
 
+const Storages = JSON.parse(localStorage.getItem(toDoStorage.TODOS_STORAGE) as any);
+
 export enum Categories {
-  "TO_DO" = "TO_DO",
-  "DOING" = "DOING",
-  "DONE" = "DONE",
+    'TO_DO' = 'TO_DO',
+    'DOING' = 'DOING',
+    'DONE' = 'DONE',
 }
 
 export interface IToDoData {
-  id: number;
-  text: string;
-  category: Categories;
+    id: number;
+    text: string;
+    category: Categories;
 }
 
 export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
+    key: 'category',
+    default: Categories.TO_DO,
 });
 
 export const toDoState = atom<IToDoData[]>({
-  key: "toDo",
-  default: JSON.parse(localStorage.getItem(toDoStorage.TODOS_STORAGE) as any),
+    key: 'toDo',
+    default: Storages ? Storages : [],
 });
 
 export const toDoSelector = selector({
-  key: "toDoSelector",
-  get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
-  },
+    key: 'toDoSelector',
+    get: ({ get }) => {
+        const toDos = get(toDoState);
+        const category = get(categoryState);
+        return toDos.filter((toDo) => toDo.category === category);
+    },
 });
--- src/components/CreateToDo.tsx
+++ src/components/CreateToDo.tsx
@@ -1,48 +1,47 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { categoryState, toDoState, toDoStorage } from "../atoms";
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useRecoilValue, useSetRecoilState } from 'recoil';
+import { categoryState, toDoState, toDoStorage } from '../atoms';
 
 interface IFormToDo {
-  toDo: string;
+    toDo: string;
 }
 
 function CreateToDo() {
-  const setToDos = useSetRecoilState(toDoState);
-  const category = useRecoilValue(categoryState);
-  const { register, handleSubmit, setValue } = useForm<IFormToDo>();
-  const onSubmit = ({ toDo }: IFormToDo) => {
-    setToDos((oldToDos) => {
-      const result = [{ id: Date.now(), text: toDo, category }, ...oldToDos];
-      localStorage.setItem(toDoStorage.TODOS_STORAGE, JSON.stringify(result));
-      return result;
-    });
-    setValue("toDo", "");
-  };
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          style={{ padding: "4px", color: "#000" }}
-          {...register("toDo", {
-            required: "텍스트를 적어주세요.",
-          })}
-          placeholder="오늘 할 일을 적어보세요."
-        />
-        <button
-          style={{
-            display: "block",
-            width: "200px",
-            height: "40px",
-            marginTop: "16px",
-            border: "1px solid #FFF",
-          }}
-        >
-          추가
-        </button>
-      </form>
-    </>
-  );
+    const setToDos = useSetRecoilState(toDoState);
+    const category = useRecoilValue(categoryState);
+    const { register, handleSubmit, setValue } = useForm<IFormToDo>();
+    const onSubmit = ({ toDo }: IFormToDo) => {
+        setToDos((oldToDos) => {
+            const result = [{ id: Date.now(), text: toDo, category }, ...oldToDos];
+            return result;
+        });
+        setValue('toDo', '');
+    };
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input
+                    style={{ padding: '4px', color: '#000' }}
+                    {...register('toDo', {
+                        required: '텍스트를 적어주세요.',
+                    })}
+                    placeholder="오늘 할 일을 적어보세요."
+                />
+                <button
+                    style={{
+                        display: 'block',
+                        width: '200px',
+                        height: '40px',
+                        marginTop: '16px',
+                        border: '1px solid #FFF',
+                    }}
+                >
+                    추가
+                </button>
+            </form>
+        </>
+    );
 }
 
 export default CreateToDo;
